fb9ea3b2866dd28a5f14ed94b47bf0255e438df5
Dhruvilbhatt <dhruvilbhattlm10@gmail.com>
2024-04-11 15:04:21 -0700

    Shell-changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,6 +4,7 @@ import blurred_bg from '../assets/blurred-bg.webp'
 import '../Styles/LandingPage.css'
 import { Element } from 'react-scroll'
 import AboutTiles from './AboutTiles'
+import Terminal from './Terminal'
 
 function LandingPage() {
     useEffect(() => {
@@ -26,7 +27,8 @@ function LandingPage() {
                     {/* <div>
                         I'm a dynamic computer science professional with a Master's degree from the University of California, Irvine, and a Bachelor's from DA-IICT, India. With a track record of success in software development, data analysis, and machine learning, I thrive on solving complex problems and delivering innovative solutions. My diverse experiences include internships at Synaptics Incorporated (CA), HuddleUp (NY), Indian Institute of Technology (IIT) - Bombay, where I engineered Azure-based applications, worked as a full-stack engineer, served as an open-source contributor, and led teams in data curation projects. Proficient in AWS (certified Cloud Practitioner), Azure, Java, Python, and more, I stay updated with the latest technologies to drive continuous improvement. Outside of work, I'm passionate about research, contributing to publications in computational finance, parallel computing, and computational plasma. I'm driven by a commitment to push the boundaries of technology and make a positive impact through collaborative projects.
                     </div> */}
-                    <AboutTiles />
+                    {/* <AboutTiles /> */}
+                    <Terminal />
                 </div>
             </div>
             <div className="landing-right">
